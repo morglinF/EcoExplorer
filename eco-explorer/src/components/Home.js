f3b958d3c60411f7278b9091350b3bc0f3131cc5
morglinF <morglin.olivierm@gmail.com>
2024-10-21 21:33:03 +0200

    API connection

--- eco-explorer/src/components/Home.js
+++ eco-explorer/src/components/Home.js
@@ -5,18 +5,32 @@ export default function Home(){
 const [animals, setAnimals] = useState([])
 
 useEffect(() => {
-    const fetchAnimalData = async() => {
-        try{
-            const response = await fetch("https://api.api-ninjas.com/v1/animals")
-            const data = await response.json()
-            setAnimals(data)
-            console.log(data)
+    const fetchAnimalData = async () => {
+        try {
+            const apiKey = process.env.REACT_APP_ANIMAL_API_KEY;
+            console.log(apiKey);
+
+            const response = await fetch(`https://api.api-ninjas.com/v1/animals?name=lion`, {
+                method: 'GET',
+                headers: {
+                    'X-Api-Key': apiKey,
+                    'Content-Type': 'application/json'
+                }
+            });
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+
+            const data = await response.json();
+            setAnimals(data);
+            console.log(data);
         } catch (error) {
-           console.error(error) 
+            console.error(error);
         }
-    }
+    };
 
     fetchAnimalData()
 }, [])
-    return <div>Homxsse</div>
+    return <div>Home</div>
 } 
